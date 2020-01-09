7eef547e861919b0bd341c8f0e291e41b7c44fb5
Nicko <aleph@Nicko-MacBook-Pro.local>
2020-01-09 23:01:32 +0700

    new template page

--- src/container/pages/ClientPages/Homepage/index.js
+++ src/container/pages/ClientPages/Homepage/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './styles.scss';
 import bannerProduct from '../../../../assets/images/dummy/hero-banner.jpg';
+import aboutUsBg from '../../../../assets/images/dummy/wave-bg.png';
 
 import { HeroBanner, ProductCardList, H2 } from '../../../../components';
 import { Header, Footer, ContactUsButton } from '../../../../components';
@@ -36,6 +37,10 @@ class HomePage extends Component {
     const ourProductBg = {
       backgroundImage: 'url(' + bannerProduct + ')',
     };
+    const aboutUs = {
+      backgroundImage: 'url(' + aboutUsBg + ')',
+    };
+
     return (
       <div className="p-homepage">
         <ContactUsButton />
@@ -43,11 +48,10 @@ class HomePage extends Component {
         <HeroBanner bannerTitle="HomePage" />
 
         <div className='about-us-section'>
-        <div className='container'>
-            <div className='header-section'>
+          <div className='section-background' style={aboutUs} alt='about-us-background'></div>
+          <div className='container'>
+            <div className='item-section'>
               <H2>About Us</H2>
-            </div>
-            <div className='content-section'>
               <p>Raven is young initiative command, which includes professionals of his activity. Each member of the commands, is not just an employee, they are something more valuable to us.  Penetrating into a fairly large and modern activity, the company Raven provides each time clearer and perfected solutions for the task.  Every employee of our company has a great experience in working with customers and solving tasks, while keeping unlimited energy and initiative.  Every client, who had come to us, did not left without a well-provided service.  Let us work together - you will not regret!</p>
             </div>
           </div>
@@ -55,18 +59,18 @@ class HomePage extends Component {
 
         <div className='our-product-section'>
           {/* <div className='section-background' style={ourProductBg} alt='product-background'></div> */}
-          <div class="curve curve-top"></div>
-          <div class="curve curve-bottom"></div>
-          <div className='container'>
-            <div className='header-section'>
-              <li className="jjj" onClick={this.handleClickNavigation}>
-                Our Product With history
-              </li>
-              <H2>Our Product</H2>
-            </div>
-            <div className='content-section'>
-              <ProductCardList history={this.props.history} />
-            </div>
+            <div class="curve curve-top"></div>
+            <div class="curve curve-bottom"></div>
+            <div className='container'>
+              <div className='header-section'>
+                <li className="jjj" onClick={this.handleClickNavigation}>
+                  Our Product With history
+                </li>
+                <H2>Our Product</H2>
+              </div>
+              <div className='content-section'>
+                <ProductCardList history={this.props.history} />
+              </div>
           </div>
         </div>
 
